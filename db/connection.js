7bff582df23e533836e388d2780f5d69166850a8
Juan David <juandamg0724@gmail.com>
2026-08-20 20:06:26 -0500

    Agrego SSL para conexion con TiDB Cloud

--- db/connection.js
+++ db/connection.js
@@ -9,7 +9,11 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  }
 });
 
 module.exports = pool;
